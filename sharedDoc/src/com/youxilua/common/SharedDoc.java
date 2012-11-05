f454350e73f3c07d3a709afa1d514a159475d750
renyuan <renyuan@minephone.com>
2012-11-05 13:02:44 +0800

    修复一个bug

--- sharedDoc/src/com/youxilua/common/SharedDoc.java
+++ sharedDoc/src/com/youxilua/common/SharedDoc.java
@@ -6,78 +6,100 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
 
+public class SharedDoc {
 
-public class SharedDoc{
-	
 	public final static String TAG = SharedDoc.class.getSimpleName();
-	
+
 	private SharedPreferences mSharedDoc;
 	private SharedPreferences.Editor sharedDocEditor;
 	private SharedPreferences tempShared;
-	public SharedDoc(SharedPreferences sharedDoc,Context ctx){
+	private SharedPreferences.Editor tempSharedEdit;
+
+	public SharedDoc(SharedPreferences sharedDoc, Context ctx) {
 		Log.d(TAG, "createt sharedDoc");
 		this.mSharedDoc = sharedDoc;
 		sharedDocEditor = mSharedDoc.edit();
-		tempShared = SharedDocFactory.getTempHashDoc(ctx);
+		tempShared = SharedDocFactory.getTempHash(ctx);
+		this.tempSharedEdit = tempShared.edit();
 	}
-	
-	private boolean checkHashCode(String key,String vaule){
+
+	private boolean checkHashCode(String key, String vaule) {
 		int currentHashCode = vaule.hashCode();
 		int valueHashCode = tempShared.getInt(key, -1);
-		if(currentHashCode == valueHashCode){
+		if (currentHashCode == valueHashCode) {
 			Log.d(TAG, "the same string");
 			return false;
-		}else{
-			tempShared.edit().putInt(key, currentHashCode).commit();
+		} else {
+			tempSharedEdit.putInt(key, currentHashCode).commit();
 			return true;
 		}
 	}
-	
+
 	public boolean insertOrUpdateDoc(String key, Object value) {
-		
+
 		if (value instanceof String) {
 			String v = (String) value;
-			if(checkHashCode(key,v)){
+			if (checkHashCode(key, v)) {
 				sharedDocEditor.putString(key, v);
-			}else{
+			} else {
 				return true;
 			}
-		
+
 		} else if (value instanceof Integer) {
 			sharedDocEditor.putInt(key, (Integer) value);
-		
+
 		} else if (value instanceof Long) {
 			sharedDocEditor.putLong(key, (Long) value);
-		
+
 		} else if (value instanceof Float) {
 			sharedDocEditor.putFloat(key, (Float) value);
-			
+
 		} else if (value instanceof Boolean) {
 			sharedDocEditor.putBoolean(key, (Boolean) value);
 		}
 		return sharedDocEditor.commit();
 	}
-	
-	
-	public Object getDataValue(String key){
-		Map<String, ?> tempData = mSharedDoc.getAll();
-		return tempData.get(key);
+
+	public String getDataValue(String key) {
+		return mSharedDoc.getString(key, null);
 	}
-	
-	public boolean removeDoc(String ... keys){
+
+	public Object getDataValue(String key, SharedType sharedType) {
+		if (sharedType != null) {
+			switch (sharedType) {
+			case BOOLEAN:
+				return Boolean.valueOf(mSharedDoc.getBoolean(key, false));
+			case INT:
+				return Integer.valueOf(mSharedDoc.getInt(key, -1));
+			case FLOAT:
+				return Float.valueOf(mSharedDoc.getFloat(key, -1f));
+			case LONG:
+				return Long.valueOf(mSharedDoc.getLong(key, -1L));
+			default:
+				return getDataValue(key);
+			}
+		} else {
+			return getDataValue(key);
+		}
+
+	}
+
+	public boolean removeDoc(String... keys) {
 		for (String removeKey : keys) {
 			sharedDocEditor.remove(removeKey);
+			tempSharedEdit.remove(removeKey);
 		}
+		tempSharedEdit.commit();
 		return sharedDocEditor.commit();
 	}
-	
+
 	public boolean clearDoc() {
+		for (String key : mSharedDoc.getAll().keySet()) {
+			tempSharedEdit.remove(key);
+		}
 		sharedDocEditor.clear();
-	    return	sharedDocEditor.commit();
+		tempSharedEdit.commit();
+		return sharedDocEditor.commit();
 	}
-
-
-	
-	
 
 }
--- sharedDoc/src/com/youxilua/common/SharedDocFactory.java
+++ sharedDoc/src/com/youxilua/common/SharedDocFactory.java
@@ -6,26 +6,38 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class SharedDocFactory {
-	
+
 	private static SharedPreferences tempShared;
 	private final static String TEMPHASHSHARED = "tempHashDoc";
-	public static SharedDoc newInstanceSharedDoc(Context ctx){
-		SharedPreferences tempShared = PreferenceManager.getDefaultSharedPreferences(ctx);
-		return new SharedDoc(tempShared,ctx);
+
+	private static SharedDoc newInstanceSharedDoc(Context ctx) {
+		SharedPreferences tempShared = PreferenceManager
+				.getDefaultSharedPreferences(ctx);
+		return new SharedDoc(tempShared, ctx);
 	}
-	
-	public static SharedDoc newInstanceSharedDoc(Context ctx,String name){
-		SharedPreferences tempShared = ctx.getSharedPreferences(name, Context.MODE_PRIVATE);
-		return new SharedDoc(tempShared,ctx);
+
+	private SharedDocFactory() {
 	}
-	
-	protected static SharedPreferences getTempHashDoc(Context ctx) {
-		if(tempShared == null){
+
+	public static SharedDoc newInstanceSharedDoc(Context ctx, String name) {
+
+		if (name == null) {
+			return newInstanceSharedDoc(ctx);
+		} else {
+			SharedPreferences tempShared = ctx.getSharedPreferences(name,
+					Context.MODE_PRIVATE);
+			return new SharedDoc(tempShared, ctx);
+		}
+
+	}
+
+	protected static SharedPreferences getTempHash(Context ctx) {
+		if (tempShared == null) {
 			Log.d(SharedDoc.TAG, "createt temp sharedDoc");
-			tempShared = ctx.getSharedPreferences(TEMPHASHSHARED, Context.MODE_PRIVATE);
+			tempShared = ctx.getSharedPreferences(TEMPHASHSHARED,
+					Context.MODE_PRIVATE);
 		}
 		return tempShared;
 	}
-	
-	
+
 }
